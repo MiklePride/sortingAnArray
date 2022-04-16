7512518fbdffe36addd9a0a8181bf456f03a26f8
Михаил Денисов <migelius.invesy@gmail.com>
2022-04-16 21:15:03 +0300

    Добавьте файлы проекта.

---
+++ sortingAnArray/Program.cs
@@ -0,0 +1,38 @@
+﻿class program
+{
+    static void Main(string[] args)
+    {
+        int[] arrayOfIntegers = new int[25];
+        int indexArrayOfIntegers;
+        int nextIndexArrayOfIntegers;
+        Random random = new Random();
+        int minimumRandomNumber = 0;
+        int maximumRandomNumber = 46;
+        int temporaryVariable;
+
+        for (indexArrayOfIntegers = 0; indexArrayOfIntegers < arrayOfIntegers.Length; indexArrayOfIntegers++)
+        {
+            arrayOfIntegers[indexArrayOfIntegers] = random.Next(minimumRandomNumber, maximumRandomNumber);
+            Console.Write(arrayOfIntegers[indexArrayOfIntegers] + " ");
+        }
+
+        for (indexArrayOfIntegers = 0; indexArrayOfIntegers < arrayOfIntegers.Length; indexArrayOfIntegers++)
+        {
+            for (nextIndexArrayOfIntegers = indexArrayOfIntegers + 1; nextIndexArrayOfIntegers < arrayOfIntegers.Length; nextIndexArrayOfIntegers++)
+            {
+                if (arrayOfIntegers[indexArrayOfIntegers] > arrayOfIntegers[nextIndexArrayOfIntegers])
+                {
+                    temporaryVariable = arrayOfIntegers[indexArrayOfIntegers];
+                    arrayOfIntegers[indexArrayOfIntegers] = arrayOfIntegers[nextIndexArrayOfIntegers];
+                    arrayOfIntegers[nextIndexArrayOfIntegers] = temporaryVariable;
+                }
+            }
+        }
+        Console.WriteLine("\n");
+
+        for (indexArrayOfIntegers = 0; indexArrayOfIntegers < arrayOfIntegers.Length; indexArrayOfIntegers++)
+        {
+            Console.Write(arrayOfIntegers[indexArrayOfIntegers] + " ");
+        }
+    }
+}
